93f48f651139f954fe44ada0857d5a262a7ba6a2
Annabelle Corp <annabelle.corp99@gmail.com>
2023-01-23 15:43:36 +1100

    Added additional object details

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Character definition
 let adventurer = {
   name: [],
   gameClass: [],
@@ -5,6 +6,7 @@ let adventurer = {
   currentHealth: 100,
 };
 
+// Enemies definition
 let goblin = {
   damage: 5,
   maxHealth: 10,
@@ -16,3 +18,55 @@ let troll = {
   maxHealth: 50,
   currentHealth: 50,
 };
+
+// Player classes definition
+
+let mageClass = {
+  weaponType: "magic",
+  damage: 20,
+  intelligence: 40, 
+  stealth: 30,
+  charisma: 10
+};
+
+let knightClass = {
+  weaponType: "sword",
+  damage: 40,
+   intelligence: 10,
+    stealth: 10,
+    charisma: 20,
+};
+
+let bardClass = {
+  weaponType: "voice",
+  damage: 10,
+   intelligence: 20,
+   stealth: 20,
+   charisma: 40,
+};
+
+let rougueClass = {
+  weaponType: "voice",
+  damage: 30,
+   intelligence: 30,
+    stealth: 40,
+    charisma: 30,
+};
+
+// In game actions
+function engageCombat(x, y){
+    
+
+}
+
+function runAway() {
+
+}
+
+function sneakPast (
+
+)
+
+function useCharisma () {
+
+}
