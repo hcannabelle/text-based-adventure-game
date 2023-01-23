f72b69b96bc2fa14739ff87198a11aaba554d582
Annabelle Corp <annabelle.corp99@gmail.com>
2023-01-23 15:17:55 +1100

    Defined some starting objects

---
+++ index.js
@@ -0,0 +1,18 @@
+let adventurer = {
+  name: [],
+  gameClass: [],
+  maxHealth: 100,
+  currentHealth: 100,
+};
+
+let goblin = {
+  damage: 5,
+  maxHealth: 10,
+  currentHealth: 10,
+};
+
+let troll = {
+  damage: 25,
+  maxHealth: 50,
+  currentHealth: 50,
+};
